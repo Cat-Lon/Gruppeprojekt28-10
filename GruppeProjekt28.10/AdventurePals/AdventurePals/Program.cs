3a7cacc0f2e8e2ffbcee0f9bc7d5b8d34dc9428f
william <william_kammer@live.dk>
2019-10-28 11:16:45 +0100

    oi
    ya coonts

--- GruppeProjekt28.10/AdventurePals/AdventurePals/Program.cs
+++ GruppeProjekt28.10/AdventurePals/AdventurePals/Program.cs
@@ -10,7 +10,7 @@ namespace AdventurePals
             Console.WriteLine("Welcome to our game: ADVENTURE PALS");
             Console.WriteLine("Hello from Poul");
             Console.WriteLine("hej fra Tommy");
-
+            Console.WriteLine("Sup bitches -Sincerely William");
         }
     }
 }
