61c3b98ad6cc4fcb488f075221d3776ce9464bbd
Tommy <tmth.skp@edu.mercantec.dk>
2019-10-28 11:12:33 +0100

    Hej

--- GruppeProjekt28.10/AdventurePals/AdventurePals/Program.cs
+++ GruppeProjekt28.10/AdventurePals/AdventurePals/Program.cs
@@ -9,6 +9,8 @@ namespace AdventurePals
 
             Console.WriteLine("Welcome to our game: ADVENTURE PALS");
             Console.WriteLine("Hello from Poul");
+            Console.WriteLine("hej fra Tommy");
+
         }
     }
 }
