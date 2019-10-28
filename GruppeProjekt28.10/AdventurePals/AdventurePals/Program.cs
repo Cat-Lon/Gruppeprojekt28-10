d67e9acd6e7a9cf0bcea22beeef52a807e7db4b4
Poul Bach <poulbachlauritsen@gmail.com>
2019-10-28 12:05:57 +0100

    Begyndelse på "player"
    variabler playername, hp og strength er oprettet, samt CreatePlayer(string) method der skal kaldes fra Menu engang.

--- GruppeProjekt28.10/AdventurePals/AdventurePals/Program.cs
+++ GruppeProjekt28.10/AdventurePals/AdventurePals/Program.cs
@@ -4,24 +4,38 @@ namespace AdventurePals
 {
     class Program
     {
+        // Player variables
+        private string playerName;
+        int playerHP;
+        int playerStrength;
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to our game: ADVENTURE PALS");
             menu();
         }
 
+        //Initializes player stats. TODO Call in Menu somewhere.
+        void CreatePlayer(string playerName)
+        {
+            this.playerName = playerName;
+            this.playerHP = 50;
+            this.playerStrength = 5;
+        }
 
-
-
-
-
-
-
-
+        int PlayerAttack()
+        {
+            return playerStrength;
+        }
 
         private static void menu()
         {
             Console.WriteLine("Welcome adventurer, please tell me your name: ");
         }
+
+        
+
+
     }
 }
